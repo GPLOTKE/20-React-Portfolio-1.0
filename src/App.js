3c58fc5d867f689c50986231bf87c9d61fa2a970
Jerry Plotke <jerry.plotke@gmail.com>
2021-08-26 14:42:30 -0500

    added routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom';
 import './App.css';
+import React from 'react';
+import Biography from './components/Biography';
+import ContactInfo from './components/ContactInfo';
+import Home from './components/Home';
+import MyWork from './components/MyWork';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <header>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/biography">Biography</Link>
+        <Link to="/mywork">My Work</Link>
+        <Link to="/contactinfo">Contact Info</Link>
+      </nav>
+      <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/biography">
+        <Biography />
+      </Route>
+      <Route path="/mywork">
+        <MyWork />
+      </Route>
+      <Route path="/contactinfo">
+        <ContactInfo />
+      </Route>
+    </Switch>
+    </header>
+    </Router>  
   );
 }
 
