c712af6757f8bb2d0c71ce09ed51730e2c6d7ee4
Jerry Plotke <jerry.plotke@gmail.com>
2021-08-26 14:42:46 -0500

    added bio page

---
+++ src/components/Biography.js
@@ -0,0 +1,7 @@
+export default function Biography () {
+    return <div>
+                <h1>Biography</h1>
+                <p>I am a new web developer currently enrolled in Northwestern's Coding Bootcamp. I have a Bachelor's degree in Marketing from Illinois State University and 7 years of sales experience in the logistics industry. I live in the suburbs just
+                    outside of Chicago with my wife, Ranna and our children, Amara and Gerald. When I'm not out spending time with my family you can usually find me at the ice rink playing hockey or looking for some live music.</p>;
+            </div>
+}
