3c9cce91e0d74464955534ec1de1aaa5c50aea3d
Jerry Plotke <jerry.plotke@gmail.com>
2021-08-26 14:43:45 -0500

    added my work page

---
+++ src/components/MyWork.js
@@ -0,0 +1,57 @@
+export default function MyWork () {
+    return <div>
+                <h1>My Work</h1>
+            
+                <div class="card">
+                    <div class="card-body">
+                        <h3 id="bootcampbox" class="card-title">BootCampBox</h3>
+                        <div class="cardlinks">
+                            <a href="https://www.nubootcampbox.xyz/">Visit BootCampBox <i class="fa fa-globe" aria-hidden="true"></i></a>
+                            <a href="https://github.com/BootcampBox/BootCampBox ">GitHub Repository <i class="fa fa-github" aria-hidden="true"></i></a>
+                        </div>
+                        <a title="Visit BootcampBox" href="https://www.nubootcampbox.xyz/">
+                            <img class="card-img-bottom" src="Assets/bootcampboxscreenshot.PNG " alt="pic of application " />
+                        </a>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-body">
+                        <h3 id="trashparty" class="card-title">TrashParty</h3>
+                        <div class="cardlinks">
+                            <a href="http://trashparty.xyz/"> Visit TrashParty <i class="fa fa-globe" aria-hidden="true"></i></a>
+                            <a href="https://github.com/GPLOTKE/16-Portfolio">GitHub Repository <i class="fa fa-github" aria-hidden="true"></i></a>
+                        </div>
+                        <a title="Visit TrashParty" href="http://trashparty.xyz/">
+                            <img class="card-img-bottom" src="Assets/trashparty.PNG" alt="pic of application " />
+                        </a>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-body">
+                        <h3 id="codequiz" class="card-title">Code Quiz</h3>
+                        <div class="cardlinks">
+                            <a href="https://gplotke.github.io/04-Homework-WebAPIs/">Visit Deployed Code Quiz <i class="fa fa-globe" aria-hidden="true"></i></a>
+                            <a href="https://github.com/GPLOTKE/04-Homework-WebAPIs ">GitHub Repository <i class="fa fa-github" aria-hidden="true"></i></a>
+                        </div>
+                        <a title="Visit Code Quiz" href="https://gplotke.github.io/04-Homework-WebAPIs/">
+                            <img class="card-img-bottom" src="Assets/codequizscreenshot.PNG " alt="pic of application" />
+                        </a>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-body">
+                        <h3 id="dayplanner" class="card-title">WD Scheduler</h3>
+                        <div class="cardlinks">
+                            <a href="https://gplotke.github.io/05-Homework-ThirdPartyAPIs/">Visit Scheduler <i class="fa fa-globe" aria-hidden="true"></i></a>
+                            <a href="https://github.com/GPLOTKE/05-Homework-ThirdPartyAPIs ">GitHub Repository <i class="fa fa-github" aria-hidden="true"></i></a>
+                        </div>
+                        <a title="Visit Scheduler" href="https://gplotke.github.io/05-Homework-ThirdPartyAPIs/">
+                            <img class="card-img-bottom" src="Assets/dayplannerscreenshot.PNG " alt="pic of application " />
+                        </a>
+                    </div>
+                </div>
+            </div>
+}
